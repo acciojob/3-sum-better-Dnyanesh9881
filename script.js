e116555b4bcc384604b9619ce358bba051d2e123
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-18 18:02:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function threeSum(arr, target) {
-return 2;
+  let ans=Number.MAX_VALUE;
+	for(let i=0;i<arr.length;i++){
+
+		for (let j = i+1; j <arr.length; j++) {
+			 for (let k = j+1; k <arr.length; k++) {
+			   let sum=arr[i]+arr[j]+arr[k];
+				 let diff=Math.abs(sum-target);
+				 if(diff<ans){
+					 ans=diff;
+				 }
+			 }
+		}
+	}
+	return ans;
   
 }
 
