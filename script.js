62d5f65978eeb7eb334a9408fabb6c2f47980901
Dnyaneshwar Suryawanshi <144836277+Dnyanesh9881@users.noreply.github.com>
2023-10-18 23:33:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 function threeSum(arr, target) {
   let ans=0;
-	let initialdiff=Infinity;
+	let initialdiff=Number.MIN_VALUE;
 	for(let i=0;i<=arr.length-3;i++){
 
-		for (let j = i+1; j <=arr.length-2; j++) {
-			 for (let k = j+1; k <=arr.length-1; k++) {
+		for (let j = i+1; j<=arr.length-2; j++) {
+			 for (let k = j+1; k<=arr.length-1; k++) {
 			   let sum=arr[i]+arr[j]+arr[k];
 				 let diff=Math.abs(sum-target);
 				 if(initialdiff>diff){
